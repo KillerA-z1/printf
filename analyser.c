cda9af00b0c2ff038c447f0a690e05144868217f
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-26 11:17:15 -0700

    Receives the main string and all the necessary parameters to

---
+++ analyser.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * analyser - Receives the main string and all the necessary parameters to
+ * print a formated string.
+ * @format: A string containing all the desired characters.
+ * @f_list: A list of all the posible functions.
+ * @arg_list: A list containing all the argumentents passed to the program.
+ * Return: A total count of the characters printed.
+ */
