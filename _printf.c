72aa53ffddb7dc6e78819b408f314062d4cfae24
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-26 10:15:58 -0700

    Handel the specifier  of integer, binary, percent

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
+ */
+int _printf(const char *format, ...)
+{ 
+	int printed_chars;
+	conver_t f_list[] = {
+		{"%", print_percent},
+		{"d", print_integer},
+		{"b", print_Binary},
+		{NULL, NULL}
+	};
+
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = analyser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
+}
+
