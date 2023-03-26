541429b00fa5490a66c2673d72211ebe7f453774
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-26 11:12:49 -0700

    function for binary,unsgined_number and print_number

--- specifier_number.c
+++ specifier_number.c
@@ -1,4 +1,98 @@
 #include "main.h"
+/**
+ * print_number - prints a number send to this function
+ * @args: List of arguments
+ * Return: The number of arguments printed
+ */
+int print_number(va_list args)
+{
+	int n;
+	int div;
+	int len;
+	unsigned int num;
+
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _write_char('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
+}
+/**
+ * print_unsgined_number - Prints an unsigned number
+ * @n: unsigned integer to be printed
+ * Return: The amount of numbers printed
+ */
+int print_unsgined_number(unsigned int n)
+{
+	int div;
+	int len;
+	unsigned int num;
+
+	div = 1;
+	len = 0;
+
+	num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
+}
+/**
+ * print_Binary - prints a binary representation of an integer
+ * @list: argument list containing the integer to be printed
+ * Return: the number of digits printed
+ */
+int print_Binary(va_list list)
+{
+    unsigned int num;
+	int i=0; 
+	int bit;
+	int count = 0;
+	num = va_arg(list, unsigned int);
+    
+
+    if (num == 0) {
+        _printf("0");
+        return (1);
+    }
+
+    while ((num >> count) > 0) {
+        count++;
+    }
+
+    for (i = count - 1; i >= 0; i--) {
+        bit = (num >> i) & 1;
+        _printf("%d", bit);
+    }
+
+    return (count);
+}
 
 /**
  * print_integer - Prints an integer
