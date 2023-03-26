2c23766e402d62275d8f4569f4cb2ba237726e9f
bazezew <bazezewbelew@gmail.com>
2023-03-26 10:09:27 -0700

    handel function integer,number,unsigned number,binary

--- main.h
+++ main.h
@@ -22,5 +22,10 @@ int _write_char(char);
 int print_char(va_list);
 int print_string(va_list);
 int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_unsgined_number(unsigned int);
+int print_Binary(va_list);
+
 
 #endif
