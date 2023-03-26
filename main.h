c6053890e88327d2537d00d7674bc11847b5f4f8
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-26 10:02:30 -0700

    handle function anyalser,_printf,cahr,string,percent

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
+
+/*Main functions*/
+int analyser(const char *format, conver_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+
+#endif
