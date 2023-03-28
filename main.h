8fcfa53c698638f937940f643eab0afe8e08b2e4
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-28 12:47:40 -0700

    fix memory_specifier

--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
 /**
  * struct convert - defines sructure for pointer charater and function.
  * @sym: pointer to char
--- memory_specifier.c
+++ memory_specifier.c
@@ -1,25 +1,45 @@
 #include "main.h"
-
 /**
  * print_pointer - Prints a pointer address in hexadecimal format.
  * @arg_list: List of arguments containing the pointer to print.
  *
  * Return: The number of characters printed.
  */
-
 int print_pointer(va_list arg_list)
 {
-	char buffer[20];
+	unsigned long int ptr = (unsigned long int)va_arg(arg_list, void *);
 	int count = 0;
-	char *ptr_buffer = buffer;
-	unsigned long int ptr = (unsigned long int) va_arg(arg_list, void*);
+	char hex_digits[] = "0123456789abcdef";
 
-	sprintf(buffer, "0x%lX", ptr);
-	while (*ptr_buffer)
+	_write_char('0');
+	_write_char('x');
+
+	if (ptr == 0)
 	{
-		_write_char(*ptr_buffer);
-		count++;
-		ptr_buffer++;
+		_write_char('0');
+		count += 3;
 	}
+	else
+	{
+		char hex_buffer[20];
+		int i = 0;
+
+		while (ptr > 0)
+		{
+			int digit = ptr % 16;
+
+			hex_buffer[i++] = hex_digits[digit];
+			ptr /= 16;
+		}
+
+		count += 2 + i;
+
+		for (i--; i >= 0; i--)
+		{
+			_write_char(hex_buffer[i]);
+		}
+	}
+
 	return (count);
 }
+
